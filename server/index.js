38eb64f72b0bf28987744a25ce33b914e4e2bcb9
Abhishek Wagare <wagareabhishek@gmail.com>
2026-05-14 09:15:19 +0530

    fixed cors for production

--- server/index.js
+++ server/index.js
@@ -12,10 +12,15 @@ const paymentRoutes = require("./routes/paymentRoutes");
 connectDB();
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ecommerce-app22.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.get("/",(req,res)=>{
